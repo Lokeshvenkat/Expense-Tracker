869b879ce44a43e762ad49e8bc1e8950036b4398
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-05-26 18:42:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import styles from "./App.css";
 
